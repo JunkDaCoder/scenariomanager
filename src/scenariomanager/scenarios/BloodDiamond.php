1a74973cdf47018879caa9f66ebd685f3bf13b54
BoyIsDino <arsenalimran@hotmail.com>
2016-10-27 10:06:58 +0100

    Update BloodDiamond.php

--- src/scenariomanager/scenarios/BloodDiamond.php
+++ src/scenariomanager/scenarios/BloodDiamond.php
@@ -1,14 +1,13 @@
 <?php
 namespace scenariomanager\scenarios;
+use pocketmine\event\entity\EntityDamageEvent;
 use scenariomanager\Main;
 use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\utils\Config;
-use pocketmine\item\Item;
 use pocketmine\event\entity\EntityDamageByBlockEvent;
 use pocketmine\block\Block;
 use pocketmine\utils\TextFormat as TF;
-class BloodDiamond
-{
+class BloodDiamond{
     private $plugin;
 
     public function __construct(Main $plugin)
@@ -24,12 +23,10 @@ class BloodDiamond
     public $scenarios;
 
     public function onBreak(BlockBreakEvent $event){
-        $block = $event->getBlock();
-        $id = $block->getId();
         $p = $event->getPlayer();
         $this->scenarios = new Config($this->plugin->getDataFolder() . "scenarios.yml");
         if ($this->scenarios->get("blooddiamond") === true) {
-            $dmg = new EntityDamageByBlockEvent(Block::get(Block::DIAMOND_ORE), $p, DAMAGE::CAUSE_CUSTOM, 1);
+            $dmg = new EntityDamageByBlockEvent(Block::get(Block::DIAMOND_ORE), $p, EntityDamageEvent::CAUSE_CUSTOM, 1);
             $p->attack(1, $dmg);
             $p->sendTip(TF::RED . "ouch");
         }
